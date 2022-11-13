f46397e8f9671707f51672df515b9f51561462ac
Lucas Oliveira <lucas@>
2022-11-12 23:52:41 -0300

    chore: implement new configuration for detox

---
+++ .detoxrc.js
@@ -0,0 +1,45 @@
+/** @type {Detox.DetoxConfig} */
+module.exports = {
+  testRunner: {
+    args: {
+      $0: 'jest',
+      config: 'e2e/jest.config.ts',
+    },
+    jest: {
+      setupTimeout: 120000,
+    },
+  },
+  apps: {
+    'android.debug': {
+      type: 'android.apk',
+      binaryPath: 'android/app/build/outputs/apk/debug/app-debug.apk',
+      build:
+        'cd android; ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug; cd -',
+      reversePorts: [8081],
+    },
+    'android.release': {
+      type: 'android.apk',
+      binaryPath: 'android/app/build/outputs/apk/release/app-release.apk',
+      build:
+        'cd android; ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release; cd -',
+    },
+  },
+  devices: {
+    emulator: {
+      type: 'android.emulator',
+      device: {
+        avdName: 'Pixel_3a_XL_API_29',
+      },
+    },
+  },
+  configurations: {
+    'android.emu.debug': {
+      device: 'emulator',
+      app: 'android.debug',
+    },
+    'android.emu.release': {
+      device: 'emulator',
+      app: 'android.release',
+    },
+  },
+};
---
+++ e2e/jest.config.ts
@@ -0,0 +1,15 @@
+import { Config } from 'jest';
+
+const config: Config = {
+  rootDir: '..',
+  testMatch: ['**/?(*.e2e.)+(spec|test).ts?(x)'],
+  testTimeout: 120000,
+  maxWorkers: 1,
+  globalSetup: 'detox/runners/jest/globalSetup',
+  globalTeardown: 'detox/runners/jest/globalTeardown',
+  reporters: ['detox/runners/jest/reporter'],
+  testEnvironment: 'detox/runners/jest/testEnvironment',
+  verbose: true,
+};
+
+export default config;
