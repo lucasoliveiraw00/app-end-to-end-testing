112323cf38022d1f6adf52984581f55c99b76abd
Lucas Oliveira <lucas@>
2022-12-07 20:10:43 -0300

    ci: implememnt test e2e android

--- .detoxrc.js
+++ .detoxrc.js
@@ -20,7 +20,6 @@ module.exports = {
     'android.release': {
       type: 'android.apk',
       binaryPath: './android/app/build/outputs/apk/release/app-release.apk',
-      testBinaryPath: './android/app/build/outputs/apk/release/app-release.apk',
       build:
         'cd android; ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release; cd -',
     },
