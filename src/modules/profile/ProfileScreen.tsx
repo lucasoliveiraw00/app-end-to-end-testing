4850fdeec00b95861a99230b1a3aeaabe7531cd2
Lucas Oliveira <lucas@>
2022-12-15 09:17:04 -0300

    ci: implememnt test e2e android

--- src/modules/profile/ProfileScreen.tsx
+++ src/modules/profile/ProfileScreen.tsx
@@ -8,7 +8,7 @@ const ProfileScreen = () => (
   <View style={styles.container} testID="ProfileScreen">
     <StatusBar style="auto" />
     <Text style={styles.title}>Profile Screen</Text>
-    <Text>versionCode 345345</Text>
+    <Text>versionCode 456456</Text>
   </View>
 );
 
