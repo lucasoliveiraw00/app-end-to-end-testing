fe3660e0d174b76c8caa2d9a2b37be50e9434d61
Lucas Oliveira <lucas@>
2023-12-21 09:44:02 -0300

    chore: implement new configuration

--- jest.config.js
+++ jest.config.js
@@ -5,12 +5,15 @@ module.exports = {
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
   ],
   setupFiles: ['./jest/setup.ts'],
-  // collectCoverage: true,
+  coverageDirectory: 'coverage',
+  collectCoverage: true,
   collectCoverageFrom: [
-    '**/*.{js,jsx}',
+    '**/*.test.{js,jsx,ts,tsx}',
+    '!**/*.{js,jsx}',
     '!**/coverage/**',
     '!**/node_modules/**',
     '!**/babel.config.js',
     '!**/jest.setup.ts',
   ],
+  testPathIgnorePatterns: ['/node_modules/', '.*e2e\\.test\\.ts'],
 };
